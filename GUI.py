ce61026a2d328afd78ee6ddef5d6fa27159534b1
MssBeast <98935913+MssBeast@users.noreply.github.com>
2022-11-06 12:26:08 +0100

    Add files via upload

--- GUI.py
+++ GUI.py
@@ -1,6 +1,5 @@
 """Final GUI """
 from tkinter import *
-import PyQt5   ####why not working huh 
 from PIL import Image, ImageTk
 import cv2
 import os 
@@ -9,32 +8,39 @@ import tkinter.filedialog as tkFileDialog
 def nothing(x):
     pass
 
+"""Creat a funtion to select an input image to use as a calibration source
+    Calirate the colors and save the calibration data as an output in csv files
+    to be used in the main code"""
 
 def select_image():
     # grab a reference to the image panels
     global panelA, panelB
 
-    # open a file chooser dialog and allow the user to select an input
-    # image
-    path = tkFileDialog.askopenfilename()
+    # open a file chooser dialog and allow the user to select an input image
+    path = tkFileDialog.askopenfilename() #save the path of the chosen image
     # ensure a file path was selected
-    if len(path) > 0:
-        # load the image from disk, convert it to grayscale, and detect
-        # edges in it
+    if len(path) > 0:  #if a file has been selected proceed
+        # load the image from disk corresponding to the saved path
         image = cv2.imread(path)
         #save image
         cv2.imwrite('paperEval.png',image)
-        os.system('python tool.py')
+        #tool script will convert the image to grayscale 
+        #detect edges in it, set and save the calibration data
+        os.system('python tool.py')  # Open python scrpt command line
 
-        # Open python scrpt command line
-
+        
+#creat function to call limits 
+#code that will allow us to devide the frame into different sections
 def limits(): 
-    os.system('python limits.py')
-def main(): 
-    os.system('python main2.py')
+    os.system('python limits.py')   # Open python scrpt command line
     
+#creat function to the main program to display all the detection and filtering   
+def main(): 
+    os.system('python main2.py')    # Open python scrpt command line
+    
+#creat function to call the comparison code to display data dispersion    
 def comparision():
-    os.system('python comparehuh.py')
+    os.system('python comparehuh2.py')  # Open python scrpt command line
     
     
     
@@ -43,13 +49,17 @@ root = Tk()
 panelA = None
 panelB = None
 
-# create a button, then when pressed, will trigger a file chooser
-# dialog and allow the user to select an input image; then add the
-# button the GUI
+
+# button to call the video with final results with all the filters applied
 btn3 = Button(root, text="show results", command=main)
 btn3.pack(side="right", fill="both", expand="yes", padx="10", pady="10")
+
+#Creat button to display graphs to compare RGB and HSV representation
 btn4 = Button(root, text="comparaison", command=comparision)
 btn4.pack(side="bottom", fill="both", expand="yes", padx="10", pady="10")
+
+# create a button, then when pressed, will trigger a file chooser
+# dialog and allow the user to select an input image; then add the
 btn = Button(root, text="Calibrate color", command=select_image)
 btn.pack(side="bottom", fill="both", expand="yes", padx="10", pady="10")
 #Button to calibrate the limits  
